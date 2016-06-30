4bac35528d02e5d5585b255d2bfb9ad56e1fb51c
Kyle Gobel <kgobel@gmail.com>
2016-06-30 01:14:56 -0400

    download the packages.config if it doesn't exist

--- build.sh
+++ build.sh
@@ -9,6 +9,7 @@ SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 TOOLS_DIR=$SCRIPT_DIR/tools
 NUGET_EXE=$TOOLS_DIR/nuget.exe
 CAKE_EXE=$TOOLS_DIR/Cake/Cake.exe
+PACKAGES_CONFIG=$TOOLS_DIR/packages.config
 
 # Define default arguments.
 SCRIPT="build.cake"
@@ -17,6 +18,7 @@ CONFIGURATION="Release"
 VERBOSITY="verbose"
 DRYRUN=false
 SHOW_VERSION=false
+
 
 # Parse arguments.
 for i in "$@"; do
@@ -39,6 +41,16 @@ if [ ! -f $NUGET_EXE ]; then
     curl -Lsfo $NUGET_EXE https://dist.nuget.org/win-x86-commandline/v3.4.4/NuGet.exe
     if [ $? -ne 0 ]; then
         echo "An error occured while downloading nuget.exe."
+        exit 1
+    fi
+fi
+
+# Download tools/packages.config if it doesn't exist
+if [ ! -f $PACKAGES_CONFIG ]; then
+    echo "Downloading packages.config"
+    curl -Lsfo $PACKAGES_CONFIG http://cakebuild.net/download/bootstrapper/packages
+    if [ $? -ne 0 ]; then
+        echo "An error occured while downloading packages.config"
         exit 1
     fi
 fi
