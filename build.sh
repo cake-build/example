024a8f2e2d78aa8d8d02917c950379d3f8a76494
Kyle Gobel <kgobel@gmail.com>
2016-06-30 01:12:52 -0400

    download a pinned 3.4.4 version of NuGet
    previous nuget.exe was a 2.8 that works with like literally nothing

--- build.sh
+++ build.sh
@@ -34,7 +34,7 @@ done
 # Download NuGet if it does not exist.
 if [ ! -f $NUGET_EXE ]; then
     echo "Downloading NuGet..."
-    curl -Lsfo $NUGET_EXE https://www.nuget.org/nuget.exe
+    curl -Lsfo $NUGET_EXE https://dist.nuget.org/win-x86-commandline/v3.4.4/NuGet.exe
     if [ $? -ne 0 ]; then
         echo "An error occured while downloading nuget.exe."
         exit 1
